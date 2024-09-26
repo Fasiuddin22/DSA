9c4c4dda2b8439f0d0dd852428c0ffb8ffe5c172
Fasiuddin22 <mdfasiuddin2210@gmail.com>
2024-09-26 19:26:42 +0530

    2nd chainges

--- java file/Searching/Linear_Search.java
+++ java file/Searching/Linear_Search.java
@@ -3,7 +3,7 @@
 public class Linear_Search {
     public static void main(String[] args) {
         int [] arr = {99,55,22,0,44,33,77,11,88};
-        int target = 98;
+        int target = 33;
         int ans = linearSearch(arr, target);
         System.out.println(ans);
     }
--- java file/simple/Practice.java
+++ java file/simple/Practice.java
@@ -5,6 +5,7 @@ public class Practice {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
+        list.add(3546);
         System.out.println(list);
     }
 }
